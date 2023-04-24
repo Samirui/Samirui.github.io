6fd75863cf69c9edb85147f8d8ae255e741d4700
Kirui Elias <eliaskirui7@gmail.com>
2023-04-24 15:38:38 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+// Define data for visualization 1
+const data1 = [
+    { year: 2000, value: 10 },
+    { year: 2001, value: 20 },
+    { year: 2002, value: 30 },
+    { year: 2003, value: 40 },
+    { year: 2004, value: 50 },
+  ];
+  
+  // Define data for visualization 2
+  const data2 = [
+    { month: "January", value: 100 },
+    { month: "February", value: 200 },
+    { month: "March", value: 300 },
+    { month: "April", value: 400 },
+    { month: "May", value: 500 },
+  ];
+  
+  // Define functions to create visualizations
+  function createVisualization1(data) 
+   
+  
